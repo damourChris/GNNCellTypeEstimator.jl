fade70de00100d1268ef4eed285b6b5866686203
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-11-17 18:10:00 +0100

    fix: dimension issues in 2nd layer of GNN model

--- src/model.jl
+++ src/model.jl
@@ -15,12 +15,9 @@ function GNNCellTypeEstimatorModel(hidden_channels)
     conv_layer = GINConv(nn1, 0.001f0)
     hidden1 = HeteroGraphConv((:gene, :to, :term) => conv_layer)
 
-    nn2 = Dense(hidden_channels => hidden_channels, relu; bias=false)
+    nn2 = Dense(hidden_channels => 1, relu; bias=false)
     conv_layer2 = GINConv(nn2, 0.001f0)
     hidden2 = HeteroGraphConv((:term, :to, :term) => conv_layer2)
-
-    conv_layer3 = GATv2Conv(hidden_channels => hidden_channels, relu; bias=false)
-    hidden3 = HeteroGraphConv((:term, :to, :term) => conv_layer3)
 
     chain = Chain(;
                   hidden1=hidden1,
