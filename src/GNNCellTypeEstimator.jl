709ae405094fa51dfd315e2a1e73f8429487f90f
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-11-17 13:55:03 +0000

    feat: organize GNNCellTypeEstimator module structure and include necessary files

--- src/GNNCellTypeEstimator.jl
+++ src/GNNCellTypeEstimator.jl
@@ -1,5 +1,26 @@
 module GNNCellTypeEstimator
 
-# Write your package code here.
+using MetaGraphs
+using Graphs
+using ExpressionData
+using GraphNeuralNetworks
+using SyntheticExpressionMixtures
+using OntologyLookup
+using OntologyTrees
+using Configurations
+using DataFrames
+using JLD2
+
+include("utils.jl")
+
+include("model.jl")
+export GNNCellTypeEstimatorModel
+
+include("train.jl")
+export TrainingStrategy, gnn_loss_function, eval_loss, train!, train_model
+
+include("prepare_data.jl")
+using .DataPreparation
+export prepare_data, DataPreparationStrategy
 
 end
